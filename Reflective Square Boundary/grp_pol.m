4dccd0b8246c9e606d2c4eaa68b638f55f19620c
Apuroopa-2001 <110373971+Apuroopa-2001@users.noreply.github.com>
2023-08-14 22:59:43 +0530

    Add files via upload

---
+++ Reflective Square Boundary/grp_pol.m
@@ -0,0 +1,42 @@
+close all
+clear
+clc
+
+%%
+
+tic
+
+load('n_pw_15_R282_k1_annulus.mat')
+
+st_time = 10;
+
+mx = zeros(no_it, (n_iter - st_time + 1)); % store x component of group polarisation (mx)
+my = zeros(no_it, (n_iter - st_time + 1)); % store y component of group polarisation (my)
+m = zeros (no_it, (n_iter - st_time + 1)); % group polarisation m = sqrt(mx^2 + my^2)
+
+for iter = 1:no_it
+
+    mx_iter = mean(cos(theta_t(:,st_time:n_iter,iter)),1); % calculate mx
+    my_iter = mean(sin(theta_t(:,st_time:n_iter,iter)),1); % calculate my 
+    m_iter = sqrt(mx_iter.^2 + my_iter.^2); % calculate m
+
+    mx(iter,:) = mx_iter;
+    my(iter,:) = my_iter;
+    m(iter,:) = m_iter;
+
+end
+
+% storing mx, my and m
+mx = mx.';
+mx = mx(:);
+my = my.';
+my = my(:);
+m = m.';
+m = m(:);
+
+ord_para = [mx my m];
+
+% storing order paramter data as [mx, my, m] in .csv format
+writematrix(ord_para, 'n_pw_15_R282_k1_annulus.csv', 'Delimiter', 'tab')
+
+toc
